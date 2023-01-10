454323bf5f37bbcf07c9fdf420fd1786f29a616f
Daniela Rodríguez <114227863+DanaroNait2002@users.noreply.github.com>
2023-01-10 18:42:21 +0000

    Commit_024

--- Assets/Script/Main Activity/Pet.cs
+++ Assets/Script/Main Activity/Pet.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class Pet : MonoBehaviour
 {
+    public enum StateSelector
+    {
+        Waiting,
+        Playing,
+    }
 
+    [Header("Default Minigame Values")]
+    [SerializeField]
+    StateSelector currentState = StateSelector.Waiting;
+
+    private void Update()
+    {
+        switch (currentState) 
+        {
+
+            case StateSelector.Waiting:
+                break;
+        }
+    }
 }
